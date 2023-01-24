2d0fb2bb8c3dc94103b752723dd036d42772a8ea
Adam Gallant <adamga@microsoft.com>
2023-01-24 10:28:55 -0500

    none

---
+++ fib-calculator/worker/index.js
@@ -0,0 +1,54 @@
+const keys = require('./keys');
+const redis = require('redis');
+const fib = require('./fib')
+
+
+const redisClient = redis.createClient({
+  // url: `redis://127.1.1.18:${keys.redisPort}`
+  url: `redis://${keys.redisHost}:${keys.redisPort}`
+});
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
+const sub = redisClient.duplicate();
+
+redisClient.connect()
+sub.connect()
+
+sub.subscribe('insert', ( message) => {
+  redisClient.hSet('values', message, fib.fib(parseInt(message)));
+});
+
+// this section is used to test out bridge to kubernetes
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", async (req, res) => {
+  res.send("<h1>hi!!</h1>")
+});
+
+
+app.get("/fib", async (req, res) => {
+  const index = req.query.index
+  if (isNaN(index)) {
+    console.log("Please provide a valid number")
+    return res.status(422).send("Please provide a valid number")
+  } 
+  number = parseInt(index)
+  if (number > 40) {
+    console.log("Index too high")
+    return res.status(422).send("Index too high");
+  }
+  const fibnumber = fib.fib(number)
+  await redisClient.hSet("values", number, fibnumber);
+    res.send(`<h1>${fibnumber}</h1>`);
+    // res.send(`hello`);
+});
+
+app.listen(5001, (err) => {
+  console.log("Listening on port 5001");
+});
